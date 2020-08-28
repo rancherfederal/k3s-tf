92fd8c2c38dd639f1aabf57fd17025f5fcb1c634
Josh Wolf <josh@joshwolf.dev>
2020-08-28 00:06:45 -0600

    simplify user inputs with data queries

--- main.tf
+++ main.tf
@@ -21,3 +21,28 @@ module "controlplane_lb" {
     "kubernetes.io/cluster/${var.name}" = "owned"
   }, var.tags)
 }
+
+#
+# Cluster Shared Security Group
+#
+resource "aws_security_group" "shared" {
+  name        = "${var.name}-k3s-shared-sg"
+  vpc_id      = var.vpc_id
+  description = "Shared k3s server/agent security group"
+
+  tags = merge({
+    "shared" = "true",
+  }, var.tags)
+}
+
+# TODO: Trim these down
+resource "aws_security_group_rule" "all-self" {
+  description       = "All traffic between nodes"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.shared.id
+  type              = "ingress"
+
+  self = true
+}
