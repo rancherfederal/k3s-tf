fe739082bbf6186c138d8a6d3f41f9045ac66722
Josh Wolf <josh@joshwolf.dev>
2020-09-01 00:23:15 -0600

    try this instead

--- modules/nodepool/files/secrets.sh
+++ modules/nodepool/files/secrets.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 set -eu
 
-while true
-  do
-    echo "Checking ${state_bucket} for ${state_key}..."
-    /usr/local/bin/aws s3api head-object --bucket "${state_bucket}" --key "${state_key}" 2>/dev/null
-    if [[ ! $? -ne 0 ]]; then
-      echo "found"
-      break
-    fi
-    echo "Going to sleep for 10 seconds..."
-    sleep 10
-done
-
 mkdir -p /var/lib/rancher/k3s
-/usr/local/bin/aws s3 cp "s3://${state_bucket}/${state_key}" /var/lib/rancher/k3s/state.env
+
+until /usr/local/bin/aws s3 cp s3://${state_bucket}/${state_key} /var/lib/rancher/k3s/state.env; do
+  echo "Waiting for ${state_key} to exist within ${state_bucket}"
+  sleep 10
+done
