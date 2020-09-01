be34db00c7001483172e6d2d88256020470ff268
Josh Wolf <josh@joshwolf.dev>
2020-08-31 23:38:45 -0600

    make the wait sliiiiightlyu better

--- modules/nodepool/files/secrets.sh
+++ modules/nodepool/files/secrets.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -eu
 
-# Poll for state to exist, error if no state exists after 20 checks with 5 second internals (100 seconds)
-/usr/local/bin/aws s3api wait object-exists --bucket ${state_bucket} --key ${state_key}
+while true
+  do
+    echo "Checking ${state_bucket} for ${state_key}..."
+    /usr/local/bin/aws s3api head-object --bucket "${state_bucket}" --key "${state_key}" 2>/dev/null
+    if [[ ! $? -ne 0 ]]; then
+      echo "found"
+      break
+    fi
+    echo "Going to sleep for 10 seconds..."
+    sleep 10
+done
 
 mkdir -p /var/lib/rancher/k3s
 /usr/local/bin/aws s3 cp "s3://${state_bucket}/${state_key}" /var/lib/rancher/k3s/state.env
